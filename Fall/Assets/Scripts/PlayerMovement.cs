2c016d8b7d74de9153ce8fe8880b24273835d422
Andy Yang <115613298+ayheong@users.noreply.github.com>
2023-11-04 21:30:10 -0700

    player movement

--- Fall/Assets/Scripts/PlayerMovement.cs
+++ Fall/Assets/Scripts/PlayerMovement.cs
@@ -11,19 +11,13 @@ public class PlayerMovement : MonoBehaviour
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
 
-    private float dirX = 0f;
-    [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float maxSpeed = 7f;
-    private float holdTime = 0f;
-    private float initialSpeed;
-    private float lastDir = 0f;
-    private float duration = 1f;
-    private float reverseFactor = 1f;
+    private float duration = .5f;
+
 
     // Start is called before the first frame update
     private void Start()
     {
-        initialSpeed = moveSpeed;
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
@@ -32,79 +26,49 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        reverseFactor = 1f;
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
-        {
-            if (rb.velocity.x > 0f)
-            {
-                lastDir = 1f;
-            }
-            else if (rb.velocity.x < 0f)
-            {
-                lastDir = -1f;
-            }
-            else
-            {
-                lastDir = 0;
-            }
-            holdTime = 0f;
-        }
-        else if (Input.GetKey(KeyCode.A) && rb.velocity.x > 0f)
-        {
-            reverseFactor = 0.5f;
-            
-        }
-        else if (Input.GetKey(KeyCode.D) && rb.velocity.x < 0f)
-        {
-            reverseFactor = 0.5f;
-        }
-        else if (Input.GetKey(KeyCode.A) && holdTime <= 0f)
-        {
-            holdTime -= Time.deltaTime;
-        }
-        else if(Input.GetKey(KeyCode.D) && holdTime >= 0f)
-        {
-            holdTime += Time.deltaTime;
-        }
-        else
-        {
-            if(holdTime < 0f)
-            {
-                lastDir = -1f;
-            }
-            else if(holdTime > 0f)
-            {
-                lastDir = 1f;
-            }
-            /*else
-            {
-                lastDir = 0f;
-            }*/
-            holdTime = 0f;
-        }
 
-        if(holdTime != 0f)
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) && rb.velocity.x > 0f)  // pressing A and D while moving right
         {
-            moveSpeed = Mathf.Abs(holdTime);
+            duration = Mathf.Max(duration - .001f, .5f);
+            //Debug.Log("1");
         }
-        
-        if (moveSpeed > maxSpeed)
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) && rb.velocity.x < 0f)  // pressing A and D while moving left
         {
-            moveSpeed = maxSpeed;
+            duration = Mathf.Min(duration + .001f, .5f);
+            //Debug.Log("2");
         }
-
-        dirX = Input.GetAxisRaw("Horizontal");
-        if(holdTime == 0f && moveSpeed > 1.00001f)
+        else if (Input.GetKey(KeyCode.A) && rb.velocity.x > 0f)  // pressing A while moving right
         {
-            dirX = lastDir;
-            //Debug.Log("movespeed before " + moveSpeed);
-            duration = 1f / (reverseFactor * moveSpeed * 100f);
-            moveSpeed = Mathf.Lerp(moveSpeed, 1f, duration);
-            //Debug.Log("movespeed after " + moveSpeed);
+            duration = Mathf.Max(duration - .003f, 0f);
+            //Debug.Log("3");
         }
-        //Debug.Log("dirx: " + dirX);
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        //Debug.Log(holdTime);
-        //Debug.Log(moveSpeed);
+        else if (Input.GetKey(KeyCode.A) && rb.velocity.x <= 0f)  // pressing A while moving left
+        {
+            duration = Mathf.Max(duration - .005f, 0f);
+            //Debug.Log("4");
+        }
+        else if (Input.GetKey(KeyCode.D) && rb.velocity.x <= 0f)  // pressing D while moving left
+        {
+            duration = Mathf.Min(duration + .003f, 1f);
+            //Debug.Log("5");
+        }
+        else if (Input.GetKey(KeyCode.D) && rb.velocity.x > 0f)  // pressing D while moving right
+        {
+            duration = Mathf.Min(duration + .005f, 1f);
+            //Debug.Log("6");
+        }
+        else if (rb.velocity.x > 0f) // no input, moving right
+        {
+            duration = Mathf.Max(duration - .001f, .5f);
+            //Debug.Log("7");
+        }
+        else if (rb.velocity.x < 0f) // no input, moving left
+        {
+            duration = Mathf.Min(duration + .001f, .5f);
+            //Debug.Log("8");
+        }
+        duration = Mathf.Clamp01(duration);
+        rb.velocity = new Vector2(Mathf.Lerp(-maxSpeed, maxSpeed, duration), rb.velocity.y);
+        Debug.Log(rb.velocity.x);
     }
 }
